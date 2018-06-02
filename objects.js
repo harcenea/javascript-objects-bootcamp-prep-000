9950c3cf6a018458a32fc8ccfa086b385a5a8e5f
Hugh A <harcenea@gmail.com>
2018-06-02 17:52:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {
-  Slowdive : "Slowdrive",
-  PhilOchs : "love",
-  MyBloodyValentine : "asf"} ;
+  "My Bloody Valentine" : "Slowdrive",
+  "Phil Ochs" : "love",
+  "Slowdive" : "asf"} ;
   
   function updatePlaylist (playlist, artistName, songTitle)
   { playlist.atmosphere="vampires" ;

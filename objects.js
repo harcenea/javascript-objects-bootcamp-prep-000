39fcf386d573f9731ff8a6e9cce824b3045bc23d
Hugh A <harcenea@gmail.com>
2018-06-02 17:42:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,9 @@
 var playlist = {
-  KKB : Heard a Song,
-  Eevee : love; 
+  KKB : "Heard a Song",
+  Eevee : "love" } ;
   
-};
+  function updatePlaylist (playlist, artistName, songTitle)
+  { playlist.atmosphere="vampires" ;
+    
+    
+  return playlist}
